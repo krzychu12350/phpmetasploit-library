af057ea6718dcfd25447424fa57d0f64a3f1ed94
Krzysztof Karaś <krzychu12350@interia.pl>
2022-04-03 00:42:02 +0200

    first commit

---
+++ src/Commands.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Krzychu12350\Phpmetasploit;
+
+class Commands
+{
+    public function msfAuth($string): string
+    {
+        return "You are connect with Metasploit Framework". $string;
+    }
+}
