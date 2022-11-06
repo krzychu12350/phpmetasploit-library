afebadb5cb43268a942e75ce58bd4c8962c94f79
Krzysztof Karaś <krzychu12350@interia.pl>
2022-11-07 00:43:37 +0100

    Added package to catching Composer Events --imporoved package versionv3

---
+++ src/ComposerScript/Installer.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace ComposerScript;
+
+use Composer\Script\Event;
+
+class Installer
+{
+    public static function postUpdate(Event $event)
+    {
+        $composer = $event->getComposer();
+        // do stuff
+    }
+
+    public static function postPackageUpdate(Event $event)
+    {
+        $packageName = $event->getOperation()
+            ->getPackage()
+            ->getName();
+        echo "$packageName\n";
+        echo "Tesssssssstttttttttt";
+        // do stuff
+    }
+
+    public static function warmCache(Event $event)
+    {
+        // make cache toasty
+    }
+}
